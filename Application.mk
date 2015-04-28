b6b4dd1abe1b45985a7c6da386a6070d6fbf0409
Antokolos <antokolos@gmail.com>
2015-04-29 01:52:53 +0600

    Added .gitignore + some tweaks

--- Application.mk
+++ Application.mk
@@ -1,2 +1,3 @@
 APP_PROJECT_PATH := $(call my-dir)/project
-APP_MODULES      := main lua SDL SDL_mixer tremor SDL_image png jpeg SDL_ttf freetype libiconv mad mikmod
+APP_BUILD_SCRIPT := $(call my-dir)/project/jni/Android.mk
+APP_MODULES      := main lua webp smpeg2 SDL2 SDL2_mixer tremor SDL2_image png jpeg SDL2_ttf freetype libiconv mad mikmod
--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-export NDK_PROJECT_PATH=./project
-/usr/local/apps/android-ndk-r10d/ndk-build
+export NDK_HOME=/usr/local/apps/android-ndk-r10d
+$NDK_HOME/ndk-build NDK_PROJECT_PATH=./project NDK_APPLICATION_MK=./Application.mk
