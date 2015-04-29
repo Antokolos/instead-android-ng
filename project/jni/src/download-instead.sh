131d8d24c466c1aa7e3fccfff4b4229b41a4d64b
Antokolos <antokolos@gmail.com>
2015-04-30 03:35:37 +0600

    Faster instead download script

--- project/jni/src/download-instead.sh
+++ project/jni/src/download-instead.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+export INSTEAD_VERSION="2.2.3"
+
 #svn checkout http://instead.googlecode.com/svn/trunk/ instead-read-only
 #mv instead-read-only instead
-git clone https://github.com/instead-hub/instead
+
+#git clone https://github.com/instead-hub/instead
+
+rm -rf instead
+curl -O -L https://github.com/instead-hub/instead/archive/$INSTEAD_VERSION.zip
+unzip $INSTEAD_VERSION.zip
+rm $INSTEAD_VERSION.zip
+mv instead-$INSTEAD_VERSION instead
