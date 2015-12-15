8807bfb26d63f899e2982ac95a9a34d136ebcc1c
Antokolos <antokolos@gmail.com>
2015-12-15 20:08:19 +0600

    dev::ING-8::Implement keyboard input -- initial

--- modules/instead_standalone/src/com/nlbhub/instead/input/InputLayout.java
+++ modules/instead_standalone/src/com/nlbhub/instead/input/InputLayout.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.ImageButton;
 import android.widget.RelativeLayout;
 import com.nlbhub.instead.R;
@@ -16,10 +17,13 @@ public class InputLayout extends RelativeLayout {
 
     private static InputDialog input;
     private ImageButton kbdButton;
+    private View view;
 
     public InputLayout(Context context) {
         super(context);
-        View view = ((Activity) context).getLayoutInflater().inflate(R.layout.inputlayout, null, false);
+        view = ((Activity) context).getLayoutInflater().inflate(R.layout.inputlayout, null, false);
+        view.setFocusable(true);
+        view.setFocusableInTouchMode(true);
         setLayoutParams(getParams());
         setGravity(Gravity.CENTER_HORIZONTAL | Gravity.BOTTOM);
         addView(view);
@@ -38,6 +42,13 @@ public class InputLayout extends RelativeLayout {
     }
 
     public void open() {
-        input.show();
+        InputMethodManager im = (InputMethodManager)getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        view.requestFocus();
+        im.showSoftInput(view, InputMethodManager.SHOW_FORCED);
+    }
+
+    public void close() {
+        InputMethodManager im = (InputMethodManager)getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        im.hideSoftInputFromWindow(view.getWindowToken(), 0);
     }
 }
