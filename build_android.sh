494fc6444dc60306974efc1f508d397df61e43f5
Antokolos <antokolos@gmail.com>
2018-01-09 18:55:40 +0600

    dev::ING-34::Support INSTEAD 3.2.0 -- trying to fix Android build script + resolved conflict for AndroidManifest.xml files

--- build_android.sh
+++ build_android.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-export ANT_HOME="/Users/Antokolos/Downloads/.programm./apache-ant-1.9.4"
-cd project
+export ANT_HOME="/Users/Antokolos/Downloads/=programm=/apache-ant-1.9.4"
+cd launcher
 $ANT_HOME/bin/ant clean release
--- update.sh
+++ update.sh
@@ -2,7 +2,14 @@
 
 export ANDROID_HOME="/usr/local/apps/android-sdk-macosx"
 
-mv ./project/build.xml ./project/build.xml.prev
+mv ./launcher/build.xml ./launcher/build.xml.prev
+mv ./modules/instead_standalone/build.xml ./modules/instead_standalone/build.xml.prev
+mv ./modules/play_apk_expansion/build.xml ./modules/play_apk_expansion/build.xml.prev
+mv ./modules/play_licensing/build.xml ./modules/play_licensing/build.xml.prev
 
-# Please note, the number '1' in '--target 1' below is the index of the android platform on your machine. To obtain it, execute 'android list targets' command
-$ANDROID_HOME/tools/android update project --target 1 --path project --name Instead
+
+# Please note, the number '2' in '--target 2' below is the index of the android platform on your machine. To obtain it, execute 'android list targets' command
+$ANDROID_HOME/tools/android update project --subprojects --target 2 --path launcher --name Instead-NG
+$ANDROID_HOME/tools/android update project --subprojects --target 2 --path modules/instead_standalone --name instead_standalone
+$ANDROID_HOME/tools/android update project --subprojects --target 2 --path modules/play_apk_expansion --name play_apk_expansion
+$ANDROID_HOME/tools/android update project --subprojects --target 2 --path modules/play_licensing --name play_licensing
