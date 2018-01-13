05aecb27113ab2fe532e27e7d49692bbe6b2e04c
Antokolos <antokolos@gmail.com>
2018-01-13 22:47:41 +0600

    dev::ING-34::Support INSTEAD 3.2.0 -- removed unused lua folder from build tree

--- Application.mk
+++ Application.mk
@@ -2,4 +2,4 @@ APP_PROJECT_PATH := $(call my-dir)/modules/instead_standalone
 APP_BUILD_SCRIPT := $(call my-dir)/modules/instead_standalone/jni/Android.mk
 NDK_TOOLCHAIN_VERSION=4.9
 APP_ABI := armeabi armeabi-v7a x86
-APP_MODULES      := main lua SDL2 SDL2_mixer SDL2_image SDL2_ttf libiconv
+APP_MODULES      := main luajit SDL2 SDL2_mixer SDL2_image SDL2_ttf libiconv
