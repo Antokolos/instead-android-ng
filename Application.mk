56315d5ae3507556406adf545fc13e72ef165371
Antokolos <antokolos@gmail.com>
2018-09-06 19:23:04 +0600

    Fixing problems with native code build

--- Application.mk
+++ Application.mk
@@ -1,5 +1,7 @@
 APP_PROJECT_PATH := $(call my-dir)/modules/instead_standalone
 APP_BUILD_SCRIPT := $(call my-dir)/modules/instead_standalone/jni/Android.mk
 NDK_TOOLCHAIN_VERSION=4.9
+APP_STL := gnustl_shared
+APP_PLATFORM := android-13
 APP_ABI := armeabi armeabi-v7a x86
 APP_MODULES      := main luajit SDL2 SDL2_mixer SDL2_image SDL2_ttf libiconv
--- modules/instead_standalone/jni/Android.mk
+++ modules/instead_standalone/jni/Android.mk
@@ -1,13 +1,6 @@
 # The namespace in Java file, with dots replaced with underscores
 SDL_JAVA_PACKAGE_PATH := com_nlbhub_instead
 
-# Path to files with application data - they should be downloaded from Internet on first app run inside
-# Java sources, or unpacked from resources (TODO)
-# Typically /sdcard/alienblaster 
-# Or /data/data/de.schwardtnet.alienblaster/files if you're planning to unpack data in application private folder
-# Your application will just set current directory there
-SDL_CURDIR_PATH := /sdcard/Instead-NG
-
 # Android Dev Phone G1 has trackball instead of cursor keys, and 
 # sends trackball movement events as rapid KeyDown/KeyUp events,
 # this will make Up/Down/Left/Right key up events with X frames delay,
